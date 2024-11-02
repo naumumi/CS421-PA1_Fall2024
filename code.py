c62bf6ba4bafe4834b6b630282bfec85dddd65fb
Umut Aktaş <91386851+naumumi@users.noreply.github.com>
2024-11-02 14:13:08 +0300

    Update code.py for testing purposes

--- code.py
+++ code.py
@@ -1,8 +1,8 @@
 # CS 421 Programming Assignment 1 - PseudoGit
 
-        def greet_the_student():
-            return "22002783"
+def greet_the_student():
+        return "123456789"
 
-        if(__name__ == "__main__"):
-            print(greet_the_student())
+if(__name__ == "__main__"):
+        print(greet_the_student())
             
